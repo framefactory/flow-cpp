c89ca98237fd4f010c444529dc76d1597f884f57
Ralph <ralph@framefactory.ch>
2014-06-12 08:52:43 +0200

    Working on GeometryLoader

---
+++ src/FlowGraphics/GeometryLoader.h
@@ -0,0 +1,55 @@
+// -----------------------------------------------------------------------------
+//  File        GeometryLoader.h
+//  Project     FlowGraphics
+// -----------------------------------------------------------------------------
+//  $Author: Ralph Wiedemeier $
+//  $Revision: 1 $
+//  $Date: 2014/06/10 $
+// -----------------------------------------------------------------------------
+
+#ifndef FLOWGRAPHICS_GEOMETRYLOADER_H
+#define FLOWGRAPHICS_GEOMETRYLOADER_H
+
+#include "FlowGraphics/Library.h"
+
+// -----------------------------------------------------------------------------
+//  Class FGeometryLoader
+// -----------------------------------------------------------------------------
+
+class FLOWGRAPHICS_EXPORT FGeometryLoader
+{
+	//  Constructors and destructor ----------------------------------
+
+public:
+	/// Default Constructor.
+	FGeometryLoader();
+	/// Virtual destructor.
+	virtual ~FGeometryLoader();
+
+	//  Public commands ----------------------------------------------
+
+public:
+
+	//  Public queries -----------------------------------------------
+
+	//  Overridables -------------------------------------------------
+
+protected:
+
+	//  Overrides ----------------------------------------------------
+
+protected:
+
+	//  Internal functions -------------------------------------------
+
+private:
+
+	//  Internal data members ----------------------------------------
+
+private:
+
+};
+	
+// -----------------------------------------------------------------------------
+
+#endif // FLOWGRAPHICS_GEOMETRYLOADER_H
